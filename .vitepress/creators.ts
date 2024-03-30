804cee5f1c0eb41b6a697877844a4339d2e3a486
limesty <limesty@limesty.moe>
2024-03-31 02:27:13 +0800

    first commit

---
+++ .vitepress/creators.ts
@@ -0,0 +1,54 @@
+export interface UserAvatar {
+  name: string
+  avatar: string
+}
+
+export interface SocialEntry {
+  type: 'github' | 'twitter' | 'email'
+  icon: string
+  link: string
+}
+
+export interface Creators {
+  avatar: string
+  name: string
+  username?: string
+  title?: string
+  org?: string
+  desc?: string
+  links?: SocialEntry[]
+  nameAliases?: string[]
+  emailAliases?: string[]
+}
+
+const creatorAvatars: Record<string, string> = {}
+
+const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
+
+export const creators: Creators[] = [
+  {
+    name: '芷沐沐',
+    avatar: creatorAvatars.limesty,
+    username: 'limesty',
+    title: 'www',
+    desc: 'www',
+    links: [
+      { type: 'github', icon: 'github', link: 'https://github.com/limesty' },
+      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/limestty' },
+    ],
+    nameAliases: ['芷沐沐'],
+    emailAliases: ['limesty@limesty.moe'],
+  },
+]
+
+export const creatorNames = creators.map(c => c.name)
+export const creatorUsernames = creators.map(c => c.username || '')
+
+export const users = creatorUsernames
+  .reduce<UserAvatar[]>((acc, name) => {
+    creatorAvatars[name] = getAvatarUrl(name)
+    acc.push({ name, avatar: creatorAvatars[name] })
+
+    return acc
+  }, [])
+  .filter(item => !!item.name)
---
+++ metadata/index.ts
@@ -0,0 +1,15 @@
+/** 文本 */
+export const siteName = 'Nólëbase'
+export const siteShortName = 'Nólëbase'
+export const siteDescription = '记录回忆，知识和畅想的地方'
+
+/** 文档所在目录 */
+export const include = ['笔记', '生活']
+
+/** Repo */
+export const githubRepoLink = 'https://github.com/limesty/knowledge'
+
+/** 无协议前缀域名 */
+export const plainTargetDomain = 'n.limesty.top'
+/** 完整域名 */
+export const targetDomain = `https://${plainTargetDomain}`
